d5a41c9be29af7326f4c2f12d500c4e8f88819fe
rishikhale <rishikhale9@gmail.com>
2025-06-23 21:09:39 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,13 @@ from sklearn.model_selection import train_test_split
 from src.exception import CustomException
 from src.logger import logging
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts', "train.csv")
@@ -59,4 +66,16 @@ if __name__ == "__main__":
     train_data, test_data = obj.initiate_data_ingestion()
     print(f"[SUCCESS] Train data saved at: {train_data}")
     print(f"[SUCCESS] Test data saved at: {test_data}")
-    print("[DEBUG] Ingestion method called.")
+    logging.info("Ingestion method called.")
+
+
+
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+
+
+
