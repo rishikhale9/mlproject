a90a744cde78703475975d50839fcd79cf8dfba7
rishikhale <rishikhale9@gmail.com>
2025-06-24 12:21:54 +0530

    Prediction Pipeline

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -64,8 +64,7 @@ class DataIngestion:
 if __name__ == "__main__":
     obj = DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
-    print(f"[SUCCESS] Train data saved at: {train_data}")
-    print(f"[SUCCESS] Test data saved at: {test_data}")
+    
     logging.info("Ingestion method called.")
 
 
